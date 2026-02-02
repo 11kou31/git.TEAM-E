23a15a575aa5aed3e71086f341e592d929301dcd
Tahono08 <h.tashiro.sys23@morijyobi.ac.jp>
2026-02-02 11:25:16 +0900

    TOPページの作成

---
+++ js/common.js
@@ -0,0 +1,13 @@
+// ヘッダー読み込み
+fetch('header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
+
+// フッター読み込み
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
