3259d8c83fab2a38f7a215d9280c006da92803d0
F.RN1945 <r.furukawa.sys23@morijyobi.ac.jp>
2026-02-08 18:50:37 +0900

    細かい修正

--- js/common.js
+++ js/common.js
@@ -1,13 +1,37 @@
-// ヘッダー読み込み
-fetch('header.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
-  });
+// common.js
 
-// フッター読み込み
-fetch('footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer').innerHTML = data;
-  });
+function loadComponent(id, file) {
+  // 現在のページが artists フォルダ内にあるかチェック
+  const isInsideFolder = window.location.pathname.includes("/artists/");
+  const pathPrefix = isInsideFolder ? "../" : "";
+
+  fetch(pathPrefix + file)
+    .then((response) => response.text())
+    .then((data) => {
+      const element = document.getElementById(id);
+      element.innerHTML = data;
+
+      // 読み込んだHTML内のリンク(href)を調整
+      if (isInsideFolder) {
+        const links = element.querySelectorAll("a");
+        links.forEach((link) => {
+          const href = link.getAttribute("href");
+          // 外部リンクや '#' でない場合に ../ を追加
+          if (
+            href &&
+            !href.startsWith("http") &&
+            !href.startsWith("#") &&
+            !href.startsWith("../")
+          ) {
+            link.setAttribute("href", "../" + href);
+          }
+        });
+      }
+    });
+}
+
+// 実行
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header", "header.html");
+  loadComponent("footer", "footer.html");
+});
